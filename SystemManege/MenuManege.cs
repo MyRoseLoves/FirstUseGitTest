05d9b3b33b563e15621bc78a588e477dff59401a
MyRoseLoves <143764850+MyRoseLoves@users.noreply.github.com>
2024-03-25 14:06:18 +0800

    Add files via upload

---
+++ SystemManege/MenuManege.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SystemManege
+{
+    public partial class MenuManege : Form
+    {
+        public MenuManege()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //Assembly currentAssem = Assembly.GetExecutingAssembly();
+
+            this.Close();
+        }
+    }
+}
